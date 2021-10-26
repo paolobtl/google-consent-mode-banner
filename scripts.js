1a596e55f53799a921e7734117569ba1c3c0be7f
Paolo <68049947+paolobtl@users.noreply.github.com>
2021-10-26 22:20:42 +0200

    cookies banner compliant with google's consent mode

--- scripts.js
+++ scripts.js
@@ -1,53 +1,103 @@
-
-/* Accetta-Rifiuta button clicks */
-function accettaTutto() {
-    window.dataLayer.push({
-        'event': 'update_consent_granted',
-    });
-    gtag('consent', 'update', {
-        'ad_storage': 'granted',
-        'analytics_storage': 'granted'
-    });
-};
-
-function rifiutaTutto() {
-    window.dataLayer.push({
-        'event': 'update_consent_denied',
-    });
-    gtag('consent', 'update', {
-        'ad_storage': 'denied',
-        'analytics_storage': 'denied'
-    });
-};
-
-
-
-
-
 /* Read checkbox value and write it in localStorage */
-var banner = document.getElementById("containerbox");
-if (localStorage.getItem('option') === null){
-    banner.setAttribute('style', 'display:block')
-}
-else {
-    banner.setAttribute('style', 'display:none')
-}
 var selection;
-var checkboxes = document.querySelectorAll("#containerbox input[type=checkbox]");
+var checkboxes = document.querySelectorAll('#containerbox input[type=checkbox]');
 checkboxes.forEach(function(el) {
     el.addEventListener('click', function(event) {
         selection = event.target.value;
         return selection;
-    })
-})
+    });
+});
 
+/* Banner showing/not showing if localStorage is not/empty */
+var banner = document.getElementById('containerbox');
+if (localStorage.getItem('option') === null) {
+    banner.setAttribute('style', 'display:block');
+} else {
+    banner.setAttribute('style', 'display:none');
+}
 
-/* Nascondi-mostra banner */
+/* Hide-show banner */
 function showIt() {
-    document.getElementById("containerbox").style.display = "block";
+    document.getElementById('containerbox').style.display = 'block';
 }
 
 function hideIt() {
-    document.getElementById("containerbox").style.opacity = "0";
-    localStorage.setItem("option", selection)
+    document.getElementById('containerbox').style.opacity = '0';
+    localStorage.setItem('option', selection);
 }
+
+
+/* Consent Default */
+function consentDefault() {
+    gtag('consent', 'default', {
+        ad_storage: 'denied',
+        analytics_storage: 'denied'
+    });
+}
+
+/* Set update command based on previous selection */
+var selezione = localStorage.getItem('selezione');
+switch (selezione) {
+    case 'granted':
+        accettaTutto();
+        break;
+    case 'denied':
+        rifiutaTutto();
+        break;
+    case 'analytics':
+        gtag('consent', 'update', {
+            ad_storage: 'denied',
+            analytics_storage: 'granted'
+        });
+        break;
+    case 'ads':
+        gtag('consent', 'update', {
+            ad_storage: 'granted',
+            analytics_storage: 'denied'
+        });
+        break;
+    default:
+        consentDefault();
+}
+/* Consent update, based on buttons selection */
+function accettaTutto() {
+    window.dataLayer.push({
+        event: 'update_consent_granted'
+    });
+    localStorage.setItem('selezione', 'granted');
+    gtag('consent', 'update', {
+        ad_storage: 'granted',
+        analytics_storage: 'granted'
+    });
+}
+
+function rifiutaTutto() {
+    window.dataLayer.push({
+        event: 'update_consent_denied'
+    });
+    localStorage.setItem('selezione', 'denied');
+    gtag('consent', 'update', {
+        ad_storage: 'denied',
+        analytics_storage: 'denied'
+    });
+}
+
+var pulsanteSelezione = document.querySelectorAll('.selezionati');
+pulsanteSelezione.forEach(function(el) {
+    el.addEventListener('click', function(event) {
+        localStorage.setItem('selezione', selection);
+        if (selection === 'analytics') {
+            gtag('consent', 'update', {
+                ad_storage: 'denied',
+                analytics_storage: 'granted'
+            });
+        } else if (selection === 'ads') {
+            gtag('consent', 'update', {
+                ad_storage: 'granted',
+                analytics_storage: 'denied'
+            });
+        } else {
+            rifiutaTutto();
+        }
+    });
+});
